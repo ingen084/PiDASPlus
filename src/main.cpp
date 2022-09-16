debc2e84a4c1ddca0fddd1fbef848effb1f398bf
ingen084 <ingen188@gmail.com>
2022-09-17 01:35:06 +0900

    インデント調整

--- src/main.cpp
+++ src/main.cpp
@@ -43,10 +43,11 @@ void printNmea(const char *format, ...)
     va_list arg;
     va_start(arg, format);
     char temp[64];
-    char* buffer = temp;
+    char *buffer = temp;
     size_t len = vsnprintf(temp, sizeof(temp), format, arg);
     va_end(arg);
-    if (len > sizeof(temp) - 1) {
+    if (len > sizeof(temp) - 1)
+    {
         buffer = new char[len + 1];
         if (!buffer)
             return;
@@ -58,7 +59,8 @@ void printNmea(const char *format, ...)
     for (int i = 0; buffer[i]; i++)
         checkSum ^= (byte)buffer[i];
     Serial.printf("$%s*%02X\r\n", buffer, checkSum);
-    if (buffer != temp) {
+    if (buffer != temp)
+    {
         delete[] buffer;
     }
     Serial.flush();
@@ -80,16 +82,16 @@ void calcOffset(uint16_t *buf)
 
 float computePGA(float *HPFilteredData)
 {
-   float pga = 0;
-   float acc = 0;
-   for ( int i = 0 ; i < samplingRate ; i++)
-   {
-      acc = sqrt(HPFilteredData[i]*HPFilteredData[i] + HPFilteredData[i+1]*HPFilteredData[i+1] + HPFilteredData[i+2]*HPFilteredData[i+2]);
-      if (acc > pga)
-        pga = acc;
-   }
+    float pga = 0;
+    float acc = 0;
+    for (int i = 0; i < samplingRate; i++)
+    {
+        acc = sqrt(HPFilteredData[i] * HPFilteredData[i] + HPFilteredData[i + 1] * HPFilteredData[i + 1] + HPFilteredData[i + 2] * HPFilteredData[i + 2]);
+        if (acc > pga)
+            pga = acc;
+    }
 
-   return pga;
+    return pga;
 }
 
 const int RETENTION_MICRO_SECONDS = 60 * 10 * 1000000;
@@ -112,48 +114,48 @@ void loop()
 
     uint16_t index = frame % bufferSize;
 
+    // 計測した値を取得
+    for (auto a = 0; a < 3; a++)
+    {
+        uint16_t v = ADC.read(a);
 
-	// 計測した値を取得
-	for (auto a = 0; a < 3; a++) {
-		
-		uint16_t v = ADC.read(a);
-		
         // 極値を弾く
         if (v < 0 || v > 4096 || v == 1024 || v == 2048 || v == 3072)
             v = (uint16_t)offset[a];
-		
-		rawData[a] = v;
-	}
 
-	// オフセット計算
-	if (!isOffsetted) {
-      calcOffset(rawData);
-      printNmea("XSOFF,0");
-      isOffsetted = true;
+        rawData[a] = v;
     }
-	
-	float offsetSample[3];
-	float newHPFilteredSample[3];
+
+    // オフセット計算
+    if (!isOffsetted)
+    {
+        calcOffset(rawData);
+        printNmea("XSOFF,0");
+        isOffsetted = true;
+    }
+
+    float offsetSample[3];
+    float newHPFilteredSample[3];
     float filteredDataSample[3];
-	
-	for (auto i = 0; i < 3; i++)
-	{
-      offsetSample[i] = (rawData[i] - offset[i]) / 1024.0f * 981;
-      newHPFilteredSample[i] = offsetSample[i];
-      filteredDataSample[i] = offsetSample[i];
-	}
 
-	FILTER.filterHP(newHPFilteredSample);
-    FILTER.filterForShindo(filteredDataSample);
-	
-	for (int i = 0 ; i < 3 ; i++)
-	{
-      HPFilteredData[index * 3 + i] = newHPFilteredSample[i];
+    for (auto i = 0; i < 3; i++)
+    {
+        offsetSample[i] = (rawData[i] - offset[i]) / 1024.0f * 981;
+        newHPFilteredSample[i] = offsetSample[i];
+        filteredDataSample[i] = offsetSample[i];
     }
-	
+
+    FILTER.filterHP(newHPFilteredSample);
+    FILTER.filterForShindo(filteredDataSample);
+
+    for (int i = 0; i < 3; i++)
+    {
+        HPFilteredData[index * 3 + i] = newHPFilteredSample[i];
+    }
+
     if (isOffsetted && ACC_REPORT_RATE > 0 && frame % (samplingRate / ACC_REPORT_RATE) == 0)
         printNmea("XSACC,%.3f,%.3f,%.3f", HPFilteredData[index * 3], HPFilteredData[index * 3 + 1], HPFilteredData[index * 3 + 2]);
-    
+
     // 3軸合成
     compositeData[index] = sqrt(
         filteredDataSample[0] * filteredDataSample[0] +
@@ -165,15 +167,15 @@ void loop()
         memcpy(compositeSortedData, compositeData, sizeof(compositeData[0]) * bufferSize);
         sortArray(compositeSortedData, bufferSize);
         auto gal = compositeSortedData[(int)(samplingRate * 0.7)];
-		float pga = computePGA(HPFilteredData);
+        float pga = computePGA(HPFilteredData);
         printNmea("XSPGA,%.3f", pga);
-		
+
         if (gal > 0)
         {
             auto rawInt = round((2.0f * log10(gal) + 0.94f) * 10.0f) / 10.0f;
             printNmea("XSINT,%.3f,%.2f", gal, rawInt);
             latestIntensity = getJmaIntensity(rawInt);
-			
+
             if (micros() - latestMaxTime > RETENTION_MICRO_SECONDS || maxIntensity <= latestIntensity)
             {
                 latestMaxTime = micros();
@@ -190,7 +192,7 @@ void loop()
 
     if (isOffsetted && frame % (samplingRate / 4) == 0 && latestIntensity < maxIntensity)
         LED.toggle(maxIntensity);
-    
+
     if (!isOffsetted)
     {
         auto v = (JmaIntensity)(offsetCounter * 100 / (samplingRate * 5) % 10);
@@ -211,7 +213,6 @@ void loop()
     // {
     //     printNmea("XTIME,%d,%d", workTime, sleepTime);
     // }
-
 
     if (digitalRead(ADJUST_PIN) && isOffsetted)
     {
