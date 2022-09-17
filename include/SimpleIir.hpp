9f42be299a0e6a20180d5048e90cd57778d02915
maki nohira <masayoshi55@live.jp>
2022-09-17 19:49:56 +0900

    バグ修正+デバッグコード

--- include/SimpleIir.hpp
+++ include/SimpleIir.hpp
@@ -25,7 +25,12 @@ public:
         for (int i = 0; i < coefBLen; i++)
             dlyX[i] = 0.0;
         for (int j = 0; j < coefALen; j++)
-            dlyY[0] = 0.0;
+        {
+            dlyY[j] = 0.0;
+#ifdef DEBUG
+            Serial.printf("dlyY:%f \r\n", dlyY);
+#endif
+        }
     }
 
     void reset()
@@ -33,7 +38,7 @@ public:
         for (int i = 0; i < coefBLen; i++)
             dlyX[i] = 0.0;
         for (int j = 0; j < coefALen; j++)
-            dlyY[0] = 0.0;
+            dlyY[j] = 0.0;
     }
 
     float filter(float input)
@@ -42,20 +47,44 @@ public:
         float acc2 = 0.0;
         /* b coeficients*/
         dlyX[0] = input;
+
+#ifdef DEBUG
+        Serial.printf("input:%f \r\n", input);
+#endif
         for (int i = 0; i < coefBLen; i++)
+        {
             acc1 += coefsB[i] * dlyX[i];
+
+#ifdef DEBUG
+            Serial.printf("coefsB[%d]:%f \r\n", i, coefsB[i]);
+            Serial.printf("dlyx[%d]:%f \r\n", i, dlyX[i]);
+            Serial.printf("acc1_incremental:%f \r\n", acc1);
+#endif
+        }
         for (int i = (coefBLen)-1; i > 0; i--)
             dlyX[i] = dlyX[i - 1];
         /* a coeficients*/
-
         for (int i = 1; i < coefALen; i++)
+        {
             acc1 -= coefsA[i] * dlyY[i];
-
+#ifdef DEBUG
+            Serial.printf("coefsA[%d]:%f \r\n", i, coefsA[i]);
+            Serial.printf("dlyY[%d]:%f \r\n", i, dlyY[i]);
+            Serial.printf("acc1_decremental:%f \r\n", acc1);
+#endif
+        }
+#ifdef DEBUG
+        Serial.printf("FINAL***\r\n acc1:%f \r\n", acc1);
+        Serial.printf("acc2:%f \r\n", acc2);
+        Serial.printf("coefsA[%d]:%f \r\n", 0, coefsA[0]);
+#endif
         dlyY[0] = (acc1 + acc2) / coefsA[0];
 
         for (int i = (coefALen)-1; i > 0; i--)
             dlyY[i] = dlyY[i - 1];
-
+#ifdef DEBUG
+        Serial.printf("dlyY[0]:%f \r\n", dlyY[0]);
+#endif
         return dlyY[0];
     }
 };
