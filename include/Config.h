5f9017059df8628225be1a46ff90c29651ae1b81
maki nohira <masayoshi55@live.jp>
2022-09-17 19:49:33 +0900

    MMA8451対応

--- include/Config.h
+++ include/Config.h
@@ -7,7 +7,7 @@
  * adjustピン
  */
 // adjust ピンを無効化する場合コメントアウトを外す
-// #define DISABLE_ADJUST_PIN
+//#define DISABLE_ADJUST_PIN
 // adjust ピンの番号
 #define ADJUST_PIN D16
 
@@ -15,7 +15,7 @@
  * 震度表記LED
  */
 // 震度表記のLEDを使用しない場合はコメントアウトを外す
-// #define DISABLE_INTENSITY_LED
+//#define DISABLE_INTENSITY_LED
 // 震度表示LEDのピンオフセット
 // オフセットを震度0として連続で10個のピンが使用される
 #define INTENSITY_LED_PIN_OFFSET D6
@@ -25,6 +25,7 @@
  * 別のADCを利用する場合はコンストラクタなどと併せて差し替えてください
  * 尚フィルタ特性の都合上、 100Hz 固定です
  */
+#ifdef USE_MCP3204
 #include "MCP3204.hpp"
 // ADCクラス
 #define ADC_CLASS MCP3204
@@ -34,3 +35,16 @@
 #define ADC_RESULT_TYPE uint16_t
 // ADCの返り値を gal に変換する式
 #define ADC_RAW_TO_GAL(i) (i) / 1024.0f * 980.665f
+#endif
+
+#ifdef USE_MMA8451
+#include "MMA8451.hpp"
+// ADCクラス
+#define ADC_CLASS MMA8451
+// ADCクラスのコンストラクタ
+#define ADC_CONSTRUCTOR MMA8451()
+// ADCの返り値に使用する型
+#define ADC_RESULT_TYPE float
+// ADCの返り値を gal に変換する式
+#define ADC_RAW_TO_GAL(i) (i)
+#endif
---
+++ include/MMA8451.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <Arduino.h>
+#include <Adafruit_MMA8451.h>
+
+class MMA8451
+{
+private:
+    Adafruit_MMA8451 mma;
+
+public:
+    MMA8451(){
+        mma = Adafruit_MMA8451();
+    };
+
+    void begin()
+    {
+        if (!mma.begin())
+        {
+            Serial.println("Couldnt start");
+            while (1)
+                ;
+        }
+        mma.setRange(MMA8451_RANGE_2_G);
+        mma.writeRegister8(0x2A, 0x00 | 0x01 | 0x04); // LNOISE
+        mma.writeRegister8(0x2B, 0x10);
+    }
+
+    void read(float *data, const float *def)
+    {
+        mma.read();
+        data[0] = mma.x_g * 100.0;
+        data[1] = mma.y_g * 100.0;
+        data[2] = mma.z_g * 100.0;
+    }
+
+    ~MMA8451(){
+        
+    };
+};
