191a8ea160b9e320dfbe69f56f2631df9e7bc629
Keiichiro <keiichiro@quitsq.com>
2023-08-20 10:41:10 +0900

    SSD1306初期化コード修正

--- include/display/SSD1306.hpp
+++ include/display/SSD1306.hpp
@@ -10,25 +10,25 @@
 #define SCREEN_ADDRESS 0x3C
 
 class SSD1306 {
-    Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+    Adafruit_SSD1306 *display;
 
 public:
     // 初期化
     void begin() {
-        this->display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
-        this->display.clearDisplay();
-        this->display.setTextSize(2);  
-        this->display.setTextColor(SSD1306_WHITE);
-        this->display.display();
+        this->display = new Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+        this->display->begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+        this->display->clearDisplay();
+        this->display->setTextColor(SSD1306_WHITE);
+        this->display->display();
     }
 
     // 起動時の状態表示
     void wakeup() {
-        this->display.clearDisplay();
-        this->display.setCursor(0, 0);
-        this->display.println("Hello, world!");
-        this->display.display();
+        this->display->clearDisplay();
+        this->display->setCursor(0, 0);
+        this->display->println("Hello, world!");
+        this->display->display();
     }
     void update() {}
     void updateIntensity() {}
-}
+};
